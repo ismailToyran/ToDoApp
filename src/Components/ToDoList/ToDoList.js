a8d8c79ff8c5c09ed6b14ff9219d2a7f8f222f94
ismail Toyran <ismailtoyran@gmail.com>
2019-05-24 14:40:28 +0300

    Checkbox Implementetion

--- src/Components/ToDoList/ToDoList.js
+++ src/Components/ToDoList/ToDoList.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done : false
+    }
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange () {
+    const _done = !this.state.done;
+    this.setState({
+      done: _done
+    })
+  }
 
   render() {
     return (
       <ul>
         { this.props.items.map((item, index) => {
           return <li key={index}>
+                   <input 
+                     type='checkbox'
+                     checked={this.state.done}
+                     onChange={this.handleOnChange}
+                     style={{ fontSize: 'x-large'}}
+                   />
                    { item }
                    <a href='#' onClick={this.props.handleDelete.bind(null, item)}>
                      [x]
