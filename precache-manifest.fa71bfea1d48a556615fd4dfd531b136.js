cc508912fef1a00eefcacd70a9fe5e2a1f8a64e7
ismail Toyran <ismailtoyran@gmail.com>
2019-05-28 12:02:53 +0300

    Updates

---
+++ precache-manifest.fa71bfea1d48a556615fd4dfd531b136.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14235707498e1254176f58bc4ced49e",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "e5ef28dcefa27847558e",
+    "url": "/ToDoApp/static/css/main.5e217bca.chunk.css"
+  },
+  {
+    "revision": "f732b27740101701aef5",
+    "url": "/ToDoApp/static/js/2.5fc96607.chunk.js"
+  },
+  {
+    "revision": "e5ef28dcefa27847558e",
+    "url": "/ToDoApp/static/js/main.857d8a21.chunk.js"
+  },
+  {
+    "revision": "fb1067834985d33aa2a1",
+    "url": "/ToDoApp/static/js/runtime~main.f2c45213.js"
+  }
+]);
